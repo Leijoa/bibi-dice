577e008445602c40d5350e66edc69c6fe0b1ad72
Leijoa <67219619+Leijoa@users.noreply.github.com>
2026-05-07 14:06:43 +0800

    Delete run_tests.js

--- run_tests.js
+++
@@ -1,6 +0,0 @@
-import { calculateEngineScore } from './js/engine.js';
-import assert from 'assert';
-
-console.log('Testing engine score for Group D...');
-// just dummy check for now
-console.log('Passed dummy tests');
